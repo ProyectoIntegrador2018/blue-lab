992b5040c1ca83a229336b02a689f053264047e4
Esthephany Ayala <esthephany4@hotmail.com>
2020-01-24 02:33:50 -0600

    detalles del item, falta cambiar img de proyecto

--- showroom/src/App.vue
+++ showroom/src/App.vue
@@ -1,128 +1,26 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">
-        <img class ="logo-img" src="./assets/BPLogoTAG_RGB-72dpi_Ver01-Blanco.png">
-      </a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav text-uppercase ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#services">Financiero</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#portfolio">Comercial</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">Educativo</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#team">Administrativo</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#contact">Transporte</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    </nav>
+  <div class='home'>
+    <NavBar></NavBar>
+    <Header></Header>
+    <DetailsItem></DetailsItem>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+import Header from '@/components/Header.vue'
+import DetailsItem from '@/components/DetailsItem.vue'
+
+export default {
+  name: 'home',
+  components: {
+    NavBar,
+    Header,
+     DetailsItem
+  }
 }
+</script>
 
-#nav {
-  padding: 5px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-#mainNav {
-    background-image: url("./assets/wallpaper.jpg");
-     height: 12%;
-  }
-  
-  #mainNav .navbar-toggler {
-    font-size: 0px;
-    right: 0;
-    padding: 5px;
-   
-    text-transform: uppercase;
-    color: white;
-    border: 0;
-    background-color: #333366;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  #mainNav .navbar-brand {
-    color: #0370fe;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  #mainNav .navbar-brand.active, #mainNav .navbar-brand:active, #mainNav .navbar-brand:focus, #mainNav .navbar-brand:hover {
-    color: #0370fe;
-  }
-  
-  #mainNav .navbar-nav .nav-item .nav-link {
-    font-size: 90%;
-    font-weight: 400;
-    padding: 10px;
-    letter-spacing: 1px;
-    color: white;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-  }
-  
-  #mainNav .navbar-nav .nav-item .nav-link.active, #mainNav .navbar-nav .nav-item .nav-link:hover {
-    color: #333366;
-  }
-
-  .logo-img{
-    display: flex;
-    width: 120px;
-    height: 70px;
-    
-  }
-  
-  @media (min-width: 992px) {
-    #mainNav {
-      padding-top: 25px;
-      padding-bottom: 25px;
-      transition: padding-top 0.3s, padding-bottom 0.3s;
-      border: none;
-      background-color: transparent;
-    }
-    #mainNav .navbar-brand {
-      font-size: 1.75em;
-      transition: all 0.3s;
-    }
-    #mainNav .navbar-nav .nav-item .nav-link {
-      padding: 1.1em 1em !important;
-    }
-    #mainNav.navbar-shrink {
-      padding-top: 0;
-      padding-bottom: 0;
-    
-    }
-    #mainNav.navbar-shrink .navbar-brand {
-      font-size: 1.25em;
-      padding: 12px 0;
-    }
-  }
-
-</style>
---
+++ showroom/src/components/DetailsItem.vue
@@ -0,0 +1,225 @@
+<template>
+
+<div class="detailItem">
+    <div class="featureItem">
+    <img src= "../assets/blueppl.png" class="imgFeature" >
+    </div>
+
+     <div class="tabsItem">
+    
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item">
+             <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Info 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Info 2</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Info 3</a>
+        </li>
+        </ul>
+
+
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</div>
+ 
+            </div> 
+    </div> <!-- div tabsItem -->
+
+<div class= "extrasItem">
+    
+    <div class="certif">
+        <img src="../assets/aws-certif.png" alt="aws" class="aws-img">
+        <img src="../assets/iso-certif.png" alt="iso" class="iso-img">
+        <img src="../assets/microsoft-certif.png" alt="certif" class="microsoft-img">
+        <img src="../assets/istob-certf.png" alt="istob" class="istob-img">
+
+    </div>
+
+    <div class="salesContact">
+        <div class="contactImg">
+            <img src ="../assets/user-def.png" class="user-img">
+        </div>
+        <div class="contactInfo">
+        <div class="user-div">
+        <img src="../assets/user.svg" class="user">
+        <p> German de la Fuente </p>
+        </div>
+
+        <div class="phone-div">
+        <img src="../assets/phone.svg" class="phone">
+        <p> 8123456789 </p>
+        </div>
+
+        <div class="mail-div">
+        <img src="../assets/mail.svg" class="mail">
+        <p> gdelafuente@bluepeople.com </p>
+        </div>
+
+        </div>
+    </div> <!-- div salesContact -->
+
+    <div class= "tags">
+        <span class="badge badge-info">Info</span>
+        <span class="badge badge-info">Info</span>
+        <span class="badge badge-info">Info</span>
+        <span class="badge badge-info">Info</span>
+        
+    </div> <!-- div tags -->
+</div> <!-- div extrasItem -->
+
+</div> <!-- div detailItem -->
+
+</template>
+
+
+<script>
+export default {
+  name: 'DetailsItem',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+.detailItem{
+    display:  flex;
+   
+   padding: 10px;
+   margin: 5px;
+   flex-direction: row;
+   background-color: #060e3b;
+   border-radius:5px;
+   
+}
+
+.tabsItem{
+    width: 70%;
+}
+.imgFeature {
+  border-radius: 10px;
+}
+
+.certif{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    
+}
+
+.aws-img{
+    width: 85px;
+    height: 40px;
+    margin: 15px;
+    margin-top: 5px;
+}
+.microsoft-img{
+    width: 120px;
+    height: 30px;
+    margin: 15px;
+    margin-top: 5px;
+}
+.istob-img{
+    width: 70px;
+    height: 40px;
+    margin: 15px;
+    margin-top: 5px;
+}
+.iso-img{
+    width: 120px;
+    height: 40px;
+    margin: 15px;
+    margin-top: 5px;
+}
+
+
+
+.nav-tabs{
+  background-color:#060e3b;
+   margin: 30px;
+}
+.tab-content{
+    background-color:#060e3b;
+    color:white;
+    padding-left:30px;
+    padding-top: 20px;
+    width: 90%;
+}
+.nav-tabs > li > a{
+  border: medium none;
+   background-color: #060e3b !important;
+   color: white;
+}
+.nav-tabs > li > a:hover{
+  background-color: white !important;
+    border: medium none;
+    border-radius: 0;
+    color:black;
+}
+a.active.nav-link{
+    color: #17a2b8!important;
+    font: bolder;
+}
+
+.tags{
+    display: flex;
+    flex-direction: row;
+    
+    padding: 30px;
+}
+.extrasItem{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin-top:30px;
+    margin-bottom: 30px;
+    align-items: baseline;
+}
+
+span{
+    margin: 5px;
+    
+}
+
+.user-img{
+    border-radius: 50%;
+    width:90px;
+}
+
+.salesContact{
+    display: flex;
+    flex-direction: row;
+}
+
+.contactInfo{
+display: flex;
+flex-direction: column;
+margin-top: 10px;
+margin-left: 20px;
+color: white;
+align-items: baseline;
+flex-wrap: wrap;
+
+}
+
+.user-div, .phone-div, .mail-div{
+    display: flex;
+    flex-direction: row;
+}
+p{
+    margin: 20px;
+}
+.phone, .user, .mail{
+    height: 20px;
+    filter: brightness(0) invert(1);
+  
+}
+</style>
