14dbc55a08cc34a1c35ba3ee71b3263398d945d7
Alberto Bermea <alberto-bermi@hotmail.com>
2020-01-26 20:35:10 -0600

    Read, Delete, Update database (#47)
    * Routes to read, update, delete items
    
    * Read, Delete and Update of database

--- showroom_api/src/routes.js
+++ showroom_api/src/routes.js
@@ -4,6 +4,15 @@ const router = express.Router()
 const items = require('./controllers/items.js');
 
 router.post('/item',items.createItem)
+router.patch('/item/:id',items.updateItem)
+router.get('/items',items.getItems)
+router.get('/item/:id',items.getItem)
+router.get('/itemsTag/:tag',items.getItemsByTag)
+router.get('/itemsTitle/:title',items.getItemsByTitle)
+router.delete('/item/:id',items.deleteItem)
+router.get('/tags',items.getDistinctTags)
+router.get('/titles',items.getTitles)
+
 
 router.get('*', function(req, res) {
   res.send({
