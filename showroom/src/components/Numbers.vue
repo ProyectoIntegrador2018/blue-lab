4c6b21cfa53cb9bf399b0c6e82f656d8a3cfa1ff
Doigo12 <doigo_nosecomoestas@hotmail.com>
2020-01-31 23:06:13 -0600

    Se modificaron que los numeros del home page cargen directo desde la base de datos (#65)

--- showroom/src/components/Numbers.vue
+++ showroom/src/components/Numbers.vue
@@ -1,9 +1,17 @@
 <template>
 	<div class="container">
 		<div class="row h-100">
-			<div class="num col-sm" v-for="number in numbers" :key="number.type">
-				<p>{{number.value}}</p>
-				<p class="num-title">{{number.description}}</p>
+			<div class="num col-sm">
+				<p>{{cont_1}}</p>
+				<p class="num-title">{{title_cont_1}}</p>
+			</div>
+			<div class="num col-sm">
+				<p>{{cont_2}}</p>
+				<p class="num-title">{{title_cont_2}}</p>
+			</div>
+			<div class="num col-sm">
+				<p>{{cont_3}}</p>
+				<p class="num-title">{{title_cont_3}}</p>
 			</div>
 		</div>
 	</div>
@@ -16,13 +24,14 @@ import {mapState} from 'vuex'
 
 	export default{
 		name:"Numbers",
-		data: () => ({
-			numbers: [
-			{type:"1",value:"88",description:"Proyectos"},
-			{type:"2",value:"55",description:"Desarrolladores"},
-			{type:"3",value:"99",description:"Certificados"}
-			]
-		})
+		// data: () => ({
+		// 	numbers: [
+		// 	{type:"1",value:"88",description:"Proyectos"},
+		// 	{type:"2",value:"55",description:"Desarrolladores"},
+		// 	{type:"3",value:"99",description:"Certificados"}
+		// 	]
+		// })
+		computed: mapState(['title_cont_1','title_cont_2','title_cont_3','cont_1','cont_2','cont_3']),
 	}
 </script>
 
