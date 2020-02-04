b12e491e9484712747dd8229149345a5a7f951b3
Doigo12 <doigo_nosecomoestas@hotmail.com>
2020-02-04 00:32:39 -0600

    *Se agregó animacion de los numeros mediante Componente animated-numbers-vue

--- showroom/src/components/Numbers.vue
+++ showroom/src/components/Numbers.vue
@@ -2,15 +2,15 @@
 	<div class="container">
 		<div class="row h-100">
 			<div class="num col-sm">
-				<p>{{cont_1}}</p>
+				<animated-number :value="cont_1" :formatValue="func" :easing="easing" :duration="duration"/>
 				<p class="num-title">{{title_cont_1}}</p>
 			</div>
 			<div class="num col-sm">
-				<p>{{cont_2}}</p>
+				<animated-number :value="cont_2" :formatValue="func" :easing="easing" :duration="duration"/>
 				<p class="num-title">{{title_cont_2}}</p>
 			</div>
 			<div class="num col-sm">
-				<p>{{cont_3}}</p>
+				<animated-number :value="cont_3" :formatValue="func" :easing="easing" :duration="duration"/>
 				<p class="num-title">{{title_cont_3}}</p>
 			</div>
 		</div>
@@ -21,18 +21,31 @@
 
 <script>
 
-import {mapState} from 'vuex'
+import {mapState} from 'vuex';
+import AnimatedNumber from 'animated-number-vue';
 
 	export default{
 		name:"Numbers",
-		// data: () => ({
-		// 	numbers: [
-		// 	{type:"1",value:"88",description:"Proyectos"},
-		// 	{type:"2",value:"55",description:"Desarrolladores"},
-		// 	{type:"3",value:"99",description:"Certificados"}
-		// 	]
-		// })
-		computed: mapState(['title_cont_1','title_cont_2','title_cont_3','cont_1','cont_2','cont_3']),
+		components:{
+			AnimatedNumber
+		},
+		data() {
+			return{
+				easing: 'easeOutCirc',
+				duration: 1500,
+			}
+		},
+		computed:{
+			...mapState(['title_cont_1','title_cont_2','title_cont_3','cont_1','cont_2','cont_3']),
+			
+		},
+		methods:{
+			func(num){
+				return num.toFixed(0);
+			}
+		}
+		
+		
 	}
 </script>
 
@@ -43,3 +56,6 @@ import {mapState} from 'vuex'
 	}
 
 </style>
+
+
+
